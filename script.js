aeeaf6ae6ac98e33950f551645386b0f1cc4f344
yash-santosh-dhumal <140955776+yash-santosh-dhumal@users.noreply.github.com>
2024-01-04 19:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart(productName, price) {
+    // Implement your cart functionality here
+    console.log(`Added ${productName} to cart. Price: $${price}`);
+    // You might want to use localStorage or other methods to manage the cart
+  }
+  
